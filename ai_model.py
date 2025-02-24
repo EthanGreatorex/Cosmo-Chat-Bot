86fe8420a176699194852b47cd11bfa32c5d817b
EggGreatorex <eggGreatorex@gmail.com>
2025-02-24 15:02:12 +0000

    Optimised code & reduces un-needed features

--- ai_model.py
+++ ai_model.py
@@ -3,7 +3,7 @@ from groq import Groq
 
 GROQ_API_KEY = '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'
 
-def get_resp(prompt: str, context : str, prevchat : str) -> str:
+def get_resp(prompt: str, context : str, prevchat : str, mood: str) -> str:
 
     client = Groq(
         api_key=GROQ_API_KEY
@@ -14,7 +14,7 @@ def get_resp(prompt: str, context : str, prevchat : str) -> str:
             messages=[
                 {
                     "role": "user",
-                    "content": f"You are a friendly assitant named Cosmo. Your answer to any question must relate to the content of the url, markdown or user context if one is provided. Use nice formatting such as bullet points.Make use of emojis to enhance user experience but don't use a lot. You will also be given a prompt. You will also be given your previous conversation. CONTEXT: {context} PROMPT (may be a document in text form): {prompt} PREVIOUS CHATS: {prevchat}",
+                    "content": f"You are a {mood} assitant named Cosmo. Make sure your language matches your mood. Your answer to any question must relate to the content of the url, markdown, csv data or user prompts if one is provided. Use nice formatting such as bullet points.Make use of emojis to enhance user experience but don't use a lot. You will also be given a prompt. You will also be given your previous conversation. CONTEXT: {context} PROMPT (may be a document in text form): {prompt} PREVIOUS CHATS: {prevchat}",
                 }
             ],
             model="llama-3.3-70b-versatile",
--- app.py
+++ app.py
@@ -1,16 +1,135 @@
 import streamlit as st
+from ai_model import *
+import requests
+from fetch_youtube_data import *
+from pdf_to_text import *
 
-st.set_page_config(
-    page_title="Cosmo App",
-    page_icon="images/cosmo.png",
-    layout="wide",
-    initial_sidebar_state="collapsed",
+# FUNCTIONS
+@st.cache_data
+def get_pdf_text(file):
+    return convert_pdf_to_txt(file)
+
+@st.cache_data
+def get_web_data(url, type):
+    if type == 'html':
+        return crawl_website([url], type)
+    elif type == 'markdown':
+        return crawl_website([url], type)
+
+# VARIABLES
+AI_CONTEXT = ''
+
+st.set_page_config(layout='wide', page_title='Cosmo Chatbot', page_icon='./images/cosmo.png')
+
+
+if "initialized" not in st.session_state:
+    st.session_state.initialized = True
+    st.image(caption='Cosmo', image='./images/cosmo.png', width=100)
+    st.info("Hello! I'm Cosmo 😀")
+    st.info("Click the top left arrow to get started ↖️ 😀")
+    st.divider()
+
+
+with st.sidebar:
+    st.image(caption='Cosmo', image='./images/cosmo.png', width=100)
+    st.info("Hello again! Provide me with some context to make our chats more meaningful🥳")
+    st.divider()
+
+   
+    st.subheader("🌐Website URL🌐")
+    url = st.sidebar.text_input(label="Enter URL")
+    st.divider()
+
+    st.subheader("😭Comso Config😁")
+    mood_config = st.radio(label="Mood Options", options=["Happy", "Sad", "Angry"])
+    st.divider()
+    
+    st.subheader("🗃️Upload File🗃️")
+    file_upload = st.file_uploader("Upload your file:", type=['pdf', 'csv', 'txt'])
+
+    st.divider()
+    st.write("###### _Original logo design created by stockgiu on [FreePik](%s)_" % 'https://www.freepik.com/author/stockgiu')
+
+    
+    if url:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                st.success("Website verified!")
+        except Exception:
+            st.error("That webpage doesn't seem to exist.")
+        AI_CONTEXT = url
+
+  
+    if file_upload:
+        with st.spinner("Processing file..."):
+            if file_upload.type == 'application/pdf':
+                text = get_pdf_text(file_upload)
+                AI_CONTEXT = text
+            elif file_upload.type == 'text/plain':
+                AI_CONTEXT = file_upload.read().decode('utf-8')
+            elif file_upload.type == 'text/csv':
+                AI_CONTEXT = file_upload.read().decode('utf-8')
+        st.success("File processed successfully!")
+
+
+st.markdown(
+    """
+<style>
+    .st-emotion-cache-janbn0 {
+        flex-direction: row-reverse;
+        text-align: right;
+    }
+</style>
+""",
+    unsafe_allow_html=True,
 )
 
-st.image(image='images/cosmo.png', width=150)
-st.info("Hello there! I'm Cosmo 😀")
-st.info("I'm here to help you with any questions you may have! 🤔")
-st.info("Use the menu on the left to navigate to the desired functionality! ↖️")
-st.divider()
 
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "assistant", "content": "Send me a message and I'll respond!"}]
+
+# Display the chat history
+for message in st.session_state.messages:
+    if message['role'] == 'assistant':
+        with st.chat_message(message["role"], avatar='./images/cosmo.png'):
+            st.markdown(message["content"])
+    else:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+
+prompt = st.chat_input("Type message...")
+
+
+if prompt:
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Process the assistant's response
+    with st.chat_message("assistant", avatar='./images/cosmo.png'):
+        match mood_config:
+            case "Happy":
+                mood = "happy"
+            case "Sad":
+                mood = "sad"
+            case "Angry":  
+                mood = "angry"
+            case _:
+                mood = "happy"
+
+        response, tokens_used, time_taken = get_resp(prompt, AI_CONTEXT, st.session_state.messages, mood)
+
+        if response == 'ERROR':
+            st.markdown("Whoops! I think your input was too large")
+        elif response == 'TOKENS USED':
+            st.markdown("Whoops! I think I've run out of tokens for today")
+        else:
+            st.markdown(response)
+            st.markdown(f"Tokens used: {tokens_used}")
+            st.markdown(f"Time taken: {time_taken}")
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
 
